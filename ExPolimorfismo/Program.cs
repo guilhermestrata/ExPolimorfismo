a7541718bd647ad5e2e4af03f7c9234dd511b84c
Guilherme Arendt <guiarendt006@gmail.com>
2023-05-01 21:22:14 -0300

    Add project files.

---
+++ ExPolimorfismo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Collections.Generic;
+using ExPolimorfismo.Entities;
+
+namespace ExPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} Data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char yn = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(yn == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employees.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, hours, valuePerHour)); 
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach(Employee emp in employees)
+            {
+                Console.WriteLine($"{emp.Name} - ${emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            
+        }
+    }
+}
